a6a67578c9ec9f6eaf0ba459c271e3579e4db8d6
冷曦 <2218872014@qq.com>
2024-07-07 19:50:14 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import fs from node:fs
+
+//输出提示
+logger.info('air插件正在编写中')
+
+//加载插件
+const files = fs.readdirSync('./plugins/air-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`))
+})
+
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+
+  if (ret[i].status != 'fulfilled') {
+      logger.error(`载入插件错误：${logger.red(name)}`)
+      logger.error(ret[i].reason)
+      continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+
+
+export { apps }
